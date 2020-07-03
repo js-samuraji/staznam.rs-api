4fe7ea2bae876174d08239124a9185388759436b
Nikola <mihajlovicn44@gmail.com>
2020-07-04 00:29:59 +0200

    Add roles to user model

--- models/User.js
+++ models/User.js
@@ -1,19 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  username: String,
-  fullName: String,
-  dob: Date,
-  socialLinks: [String],
-  image: String,
-  blockList: [{ type: mongoose.ObjectId, ref: 'User' }],
-  email: String,
-  password: {
-    type: String,
-    select: false
-  }
-}, { timestamps: true });
+const userSchema = new mongoose.Schema(
+  {
+    username: String,
+    role: {
+      type: String,
+      enum: ["admin", "editor", "user"],
+      default: "user",
+    },
+    fullName: String,
+    dob: Date,
+    socialLinks: [String],
+    image: String,
+    blockList: [{ type: mongoose.ObjectId, ref: "User" }],
+    email: String,
+    password: {
+      type: String,
+      select: false,
+    },
+  },
+  { timestamps: true }
+);
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
