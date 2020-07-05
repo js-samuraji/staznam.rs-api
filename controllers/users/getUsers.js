2f43467c02a211467e83217013b59dee8e42158b
Nikola <mihajlovicn44@gmail.com>
2020-07-05 21:41:44 +0200

    Edit get users response to follow the format of other responses

--- controllers/users/getUsers.js
+++ controllers/users/getUsers.js
@@ -3,7 +3,10 @@ const User = require("../../models/User");
 const getUsers = (_req, res) => {
   User.find((err, users) => {
     if (err) return res.status(500).end();
-    res.status(200).json(users);
+    res.status(200).json({
+      sucess: true,
+      users: users
+    });
   });
 };
 
